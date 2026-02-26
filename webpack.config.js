ffd8b1f3346211d8aae8475482ed358402fca026
suren <tadevosyansuren07@gmail.com>
2026-02-26 16:50:48 +0400

    fix: copy public assets to dist for netlify

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -7,28 +8,34 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
-    clean: true
+    clean: true,
   },
   resolve: {
-    extensions: [".tsx", ".ts", ".js"]
+    extensions: [".tsx", ".ts", ".js"],
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
         use: "ts-loader",
-        exclude: /node_modules/
-      }
-    ]
+        exclude: /node_modules/,
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./public/index.html"
-    })
+      template: "./public/index.html",
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: "public/assets", to: "assets" }, // ✅ все картинки
+        { from: "public/_redirects", to: "_redirects" }, // ✅ если используешь
+      ],
+    }),
   ],
   devServer: {
     port: 3000,
     open: true,
-    historyApiFallback: true
-  }
+    historyApiFallback: true,
+  },
 };
